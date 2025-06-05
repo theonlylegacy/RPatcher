1a6322d07832a2b02b28a186372435b6717d34b6
Sero (@serotuko) <159341819+theonlylegacy@users.noreply.github.com>
2025-06-05 14:13:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ try:
     import zipfile
     import shutil
     import winshell
+    import colorama
+    import psutil
     from win32com.client import Dispatch
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
@@ -24,6 +26,8 @@ except ImportError as error:
         with open(bat_path, "w") as f:
             f.write("pip install selenium\n")
             f.write("pip install winshell\n")
+            f.write("pip install colorama\n")
+            f.write("pip install psutil\n")
     
     print("[PATCHER] Built \"requirements.bat\"!")
 
@@ -32,9 +36,20 @@ except ImportError as error:
     print("[PATCHER] Extracted libraries!")
     os.execv(sys.executable, [sys.executable] + sys.argv)
 
+def get_process(process_name):
+    process_entry = None
+
+    for entry in psutil.process_iter():
+        if entry.name() == process_name:
+            process_entry = entry
+            break
+
+    return process_entry 
+
 def get_roblox_directory():
     local_app_data = os.getenv("LOCALAPPDATA")
     directory = os.path.join(local_app_data, "Roblox", "Versions")
+    
     return directory
 
 def configure_driver(download_path, headless=True):
@@ -93,7 +108,18 @@ def cleanup_previous_versions(roblox_directory):
         if os.path.isdir(folder_path) and os.path.isfile(os.path.join(folder_path, "RobloxPlayerBeta.exe")):
             print(f"[PATCHER] Clearing previous version: \"{folder_name}\"..")
             shutil.rmtree(folder_path)
-            print(f"[PATCHER] Clearing previous version: \"{folder_name}\"!")
+            print(f"[PATCHER] Cleared previous version: \"{folder_name}\"!")
+
+def cleanup_previous_shortcuts():
+    start_menu = os.path.join(os.getenv("APPDATA"), r"Microsoft\Windows\Start Menu\Programs\Roblox")
+
+    for name in os.listdir(start_menu):
+        path = os.path.join(start_menu, name)
+
+        if name == "Roblox Player.lnk" and os.path.isfile(path):
+            print(f"[PATCHER] Clearing previous shortcut: \"{name}\"..")
+            os.remove(path)
+            print(f"[PATCHER] Cleared previous shortcut \"{name}\"!")
 
 def unzip_file(zip_filename, zip_path, extract_to):
     print(f"[PATCHER] Extracting \"{zip_filename}\"..")
@@ -104,25 +130,34 @@ def unzip_file(zip_filename, zip_path, extract_to):
     os.remove(zip_path)
     print(f"[PATCHER] Extracted \"{zip_filename}\"!")
 
-def build_shortcut(target_path, shortcut_name):
+def build_shortcut(path, name):
     start_menu = os.path.join(os.getenv("APPDATA"), r"Microsoft\Windows\Start Menu\Programs\Roblox")
     os.makedirs(start_menu, exist_ok=True)
 
-    shortcut_path = os.path.join(start_menu, f"{shortcut_name}.lnk")
-
     shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortCut(shortcut_path)
-    shortcut.Targetpath = target_path
-    shortcut.WorkingDirectory = os.path.dirname(target_path)
-    shortcut.IconLocation = target_path
+    shortcut = shell.CreateShortCut(os.path.join(start_menu, f"{name}.lnk"))
+    shortcut.Targetpath = path
+    shortcut.WorkingDirectory = os.path.dirname(path)
+    shortcut.IconLocation = path
     shortcut.save()
 
 def main():
     binary_type = input("[PATCHER] BinaryType: ")  # e.g. WindowsPlayer
     version_hash = input("[PATCHER] Version: ")  # e.g. ad3ee47cdc5e44f6
 
+    roblox_process = get_process("RobloxPlayerBeta.exe")
+
+    if roblox_process:
+        print("[PATCHER] \"RobloxPlayerBeta.exe\" is running!")
+        close_process = input("[PATCHER] Do you wish to terminate \"RobloxPlayerBeta.exe\"? (Y/N): ")
+
+        if close_process.upper() != "Y":
+            os._exit(0)
+        
+        roblox_process.terminate()
+        time.sleep(5)
+
     roblox_directory = get_roblox_directory()
-    cleanup_previous_versions(roblox_directory)
 
     api_url = f"https://rdd.fuckass.site/?channel=LIVE&binaryType={binary_type}&version={version_hash}"
     driver = configure_driver(roblox_directory)
@@ -136,6 +171,8 @@ def main():
     zip_directory = os.path.join(roblox_directory, f"version-{version_hash}")
 
     print(f"[+] Exported assembled zip file \"{zip_filename}\"!")
+
+    cleanup_previous_versions(roblox_directory)
 
     print("[PATCHER] Building version folder..")
     os.makedirs(zip_directory, exist_ok=True)
@@ -153,11 +190,13 @@ def main():
             break
 
     if roblox_player:
-        print("[PATCHER] Building shortcut..")
+        cleanup_previous_shortcuts()
+
+        print("[PATCHER] Building shortcut \"Roblox Player.lnk\"..")
         build_shortcut(roblox_player, "Roblox Player")
-        print("[PATCHER] Built shortcut!")
+        print("[PATCHER] Built shortcut \"Roblox Player.lnk\"!")
     else:
-        print("[PATCHER] Error building shortcut!")
+        print("[PATCHER] Error building shortcut \"Roblox Player.lnk\"!")
 
     print("[PATCHER] Successfully patched!")
     time.sleep(5)
